7c67e2e6d9daa5151858986164b97f814f2268b4
Bhupesh <pradhanbhupesh0@gmail.com>
2023-03-31 15:16:53 +0530

    fix: add home link in cart

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -14,9 +14,11 @@ const Cart = ({ items, quantity, total }) => {
         <div className="px-4 space-y-4">
           {items && items.map((item) => <CartItem key={item.id} item={item} />)}
         </div>
-        <button className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-400 px-4 py-2 text-sm font-medium text-white hover:bg-red-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-400 focus-visible:ring-offset-2">
-          Clear cart
-        </button>
+        <Link to="/">
+          <button className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-400 px-4 py-2 text-sm font-medium text-white hover:bg-red-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-400 focus-visible:ring-offset-2">
+            Clear cart
+          </button>
+        </Link>
       </div>
       <OrderSummary quantity={quantity} total={total} />
     </section>
