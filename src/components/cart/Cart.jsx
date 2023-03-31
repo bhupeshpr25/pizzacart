e45a5a8fe73a27fd3864262494f5995d6ab3f1b4
Bhupesh <pradhanbhupesh0@gmail.com>
2023-03-31 15:17:59 +0530

    fix: fix link error

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -6,6 +6,7 @@ import OrderSummary from "./OrderSummary";
 const Cart = ({ items, quantity, total }) => {
   console.log(items);
   const dispatch = useDispatch();
+  const { items, quantity, total } = props;
 
   return (
     <section className="w-full max-w-screen-xl mx-auto px-4 py-6 flex flex-col justify-between md:flex-row md:items-start gap-6">
@@ -14,11 +15,9 @@ const Cart = ({ items, quantity, total }) => {
         <div className="px-4 space-y-4">
           {items && items.map((item) => <CartItem key={item.id} item={item} />)}
         </div>
-        <Link to="/">
-          <button className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-400 px-4 py-2 text-sm font-medium text-white hover:bg-red-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-400 focus-visible:ring-offset-2">
-            Clear cart
-          </button>
-        </Link>
+        <button className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-400 px-4 py-2 text-sm font-medium text-white hover:bg-red-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-400 focus-visible:ring-offset-2">
+          Clear cart
+        </button>
       </div>
       <OrderSummary quantity={quantity} total={total} />
     </section>
